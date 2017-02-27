6f393663e7a2a515745ca21a47f2a0a5c3f312f9
Mihir Patel <mihir@rubensteintech.com>
2017-02-27 16:01:10 -0500

    better prompt for zsh - full path but shorter

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,14 +1,15 @@
 source $DOTFILES/zsh/git-prompt.sh
 
 PS1="%F{white}[%n@%m %~%F{green}\$(__git_ps1)%F{white}] %# "
+PATH_FORMATTER=$DOTFILES/bin/prompt-helper.pl
 
 ## color bash prompt when outside the home dir
 precmd () {
     # If outside of your home directory, print the current directory in red, blue otherwise.
     if [[ $PWD/ != /home/$USER/* ]]; then
-        PS1="%F{white}[%n@%m %K{red}%~%k%F{green}\$(__git_ps1)%F{white}] %# "
+        PS1="%F{white}[%n@%m %K{red}\$($PATH_FORMATTER)%k%F{green}\$(__git_ps1)%F{white}] %# "
     else
-        PS1="%F{white}[%n@%m %~%F{green}\$(__git_ps1)%F{white}] %# "
+        PS1="%F{white}[%n@%m \$($PATH_FORMATTER)%F{green}\$(__git_ps1)%F{white}] %# "
     fi
 }
 
