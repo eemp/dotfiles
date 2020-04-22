2444c5a7a97b42b61133baba3e82019e46f7c9ee
Mihir Patel <mpatel525@bloomberg.net>
2020-04-22 18:00:20 -0400

    feat: update zsh/prompt.zsh to tack on a machine prefix in ssh env

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,11 +5,16 @@ PATH_FORMATTER=$DOTFILES/bin/prompt-helper.pl
 
 ## color bash prompt when outside the home dir
 precmd () {
+    # detech ssh env, and thrown in a machine prefix
+    if [ -n "$SSH_CLIENT" ]; then
+        MACHINE_PREFIX="%F{red}%m%F{white}|"
+    fi
+
     # If outside of your home directory, print the current directory in red, blue otherwise.
     if [[ $PWD/ != $HOME/* ]]; then
-        PS1="%F{white}[%K{red}%m|\$($PATH_FORMATTER)%k%F{green}\$(__git_ps1)%F{white}] %# "
+        PS1="%F{white}[$MACHINE_PREFIX%K{red}\$($PATH_FORMATTER)%k%F{green}\$(__git_ps1)%F{white}] %# "
     else
-        PS1="%F{white}[%F{red}%m%F{white}|\$($PATH_FORMATTER)%F{green}\$(__git_ps1)%F{white}] %# "
+        PS1="%F{white}[$MACHINE_PREFIX\$($PATH_FORMATTER)%F{green}\$(__git_ps1)%F{white}] %# "
     fi
 }
 
